9aa990a28dd8deef3099b44613bbbb7f5fbec32c
kollert-bzz <kollert@bzz.ch>
2024-08-28 16:07:24 +0200

    10/10

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-# Dein Code kommt hier hin
+"""Let him cook"""
+import json
+
+
+def adjust_recipe(recipe, persons):
+    new_ingredients = {}
+    difference = persons / recipe['servings']
+    for ingredients, amount in recipe['ingredients'].items():
+        new_ingredients[ingredients] = amount * difference
+    new_recipe = {
+        'title': recipe['title'],
+        'ingredients': new_ingredients,
+        'servings': persons
+    }
+    return new_recipe
+
+
+def load_recipe(json_recipe):
+    return json.loads(json_recipe)
+
 
 if __name__ == '__main__':
-    # Beispiel für die Datenstruktur eines Rezepts
-    recipe_json = '{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}'
-    # Dein Code kommt hier hin
+    recipe_json = ('{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, '
+                   '"Minced Meat": 500}, "servings": 4}')
+    python_recipe = load_recipe(recipe_json)
+    adjusted_recipe = adjust_recipe(python_recipe, 2)
+    print(f'new:\n{adjusted_recipe}')
+    print(f'old:\n{python_recipe}')
